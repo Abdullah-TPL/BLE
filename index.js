802d62d3d39668f1523600b47a8a24ac56822134
Abdullah <afzalabdullah067@gmail.com>
2023-12-14 15:41:39 +0500

    Change the Code

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000;
 
 // Replace with your MongoDB Atlas connection string
-const mongoString = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
+const mongoUri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(mongoString, {})
- .then(() => console.log('Connected to MongoDB database.'))
- .catch(error => console.error('MongoDB connection error:', error));
+let client;
 
-// Define a schema for the data
-const SensorDataSchema = new mongoose.Schema({
- sensorId: String,
- timestamp: Date,
- value: Number,
-});
+// Connect to MongoDB using MongoClient
+async function connectToDatabase() {
+  try {
+    client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    console.log('Connected to MongoDB database.');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
 
-// Create a model from the schema
-const SensorData = mongoose.model('SensorData', SensorDataSchema);
-
-// Create an Express application
-const app = express();
-
-// Use body-parser middleware to parse JSON bodies
+// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 // Set up a POST route to receive data from the gateway
 app.post('/gateway', async (req, res) => {
- try {
-   // Create a new document from the incoming data
-   const sensorData = new SensorData(req.body);
+  try {
+    if (!client || !client.isConnected()) {
+      await connectToDatabase();
+    }
 
-   // Save the document to the MongoDB database
-   const savedData = await sensorData.save();
-   console.log('Data saved successfully:', savedData);
-   res.status(200).send('Data received and saved');
- } catch (error) {
-   console.error('Error saving data:', error);
-   res.status(500).send('Error saving data');
- }
+    // Access the database and collection
+    const database = client.db('BLE'); // Replace with your actual database name
+    const collection = database.collection('SensorData'); // Replace with your actual collection name
+
+    // Create a new document from the incoming data
+    const sensorData = req.body;
+
+    // Save the document to the MongoDB database
+    const result = await collection.insertOne(sensorData);
+
+    console.log('Data saved successfully:', result);
+    res.status(200).send('Data received and saved');
+  } catch (error) {
+    console.error('Error saving data:', error);
+    res.status(500).send('Error saving data');
+  }
 });
 
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
- console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
